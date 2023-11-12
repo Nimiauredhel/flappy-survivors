a8d71207df9192b4ebc005c562cf571b8004100a
Mickey Hoz <nimiauredhel@gmail.com>
2023-11-12 18:46:01 +0200

    [build error workaround]

---
+++ Assets/Scripts/Gameplay/Weapons/WeaponLogic/WeaponLogicSandbox.cs
@@ -0,0 +1,21 @@
+using System;
+using Gameplay.Configuration;
+using UnityEngine;
+
+namespace Gameplay.Weapons
+{
+    [Serializable]
+    public class WeaponLogicSandbox
+    {
+        public virtual void Initialize(WeaponInstance instance){}
+        
+        public virtual void OnDispose(WeaponInstance instance){}
+        
+        public virtual void Draw(WeaponInstance instance){}
+        
+        public virtual void Sheathe(WeaponInstance instance){}
+        
+        public virtual void HitHandler(object sender, Collider2D other, WeaponInstance instance){}
+
+    }
+}
