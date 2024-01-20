a7dbf53c1dda314fb2ebc4cd1338362f8b417122
Mickey Hoz <nimiauredhel@gmail.com>
2024-01-20 15:43:15 +0200

    tweaked intro to avoid sound leakage into the next scene

--- Assets/Scripts/Intro/PlayIntroJingle.cs
+++ Assets/Scripts/Intro/PlayIntroJingle.cs
@@ -1,15 +1,26 @@
+using System;
+using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 namespace Intro
 {
     public class IntroJingle : MonoBehaviour
     {
         [SerializeField] private EventReference introJingle;
+        private EventInstance introJingleInstance;
         
-        public void Start()
+        private void OnEnable()
         {
-            RuntimeManager.PlayOneShot(introJingle);
+            introJingleInstance = RuntimeManager.CreateInstance(introJingle);
+            introJingleInstance.start();
+        }
+
+        private void OnDisable()
+        {
+            introJingleInstance.stop(STOP_MODE.ALLOWFADEOUT);
+            introJingleInstance.release();
         }
     }
 }
